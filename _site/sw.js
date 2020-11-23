53391dee7996a1e51459b5e76fb1dbc343823093
Pedro Torrealba B <pedro@mego.cl>
2020-11-23 17:07:19 -0300

    q

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201123085722";
+var CACHE_NAME = "saikiransripada-20201123091942";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202011230857",
+        "/css/style.css?202011230919",
         "/"
       ]);
     })
