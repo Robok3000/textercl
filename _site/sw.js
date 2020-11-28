76b460705b94643eb5d62505d9cfd6aa67c7a4a2
Pedro Torrealba B <pedro@mego.cl>
2020-11-28 09:35:14 -0300

    fix typo

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201123091942";
+var CACHE_NAME = "saikiransripada-20201128093456";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202011230919",
+        "/css/style.css?202011280934",
         "/"
       ]);
     })
