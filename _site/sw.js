31e0302669c9b42224a5d704061bcdf2dd50e9cc
Pedro Torrealba B <pedro@mego.cl>
2020-11-28 15:59:17 -0300

    new favicon

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201128152544";
+var CACHE_NAME = "saikiransripada-20201128153853";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202011281525",
+        "/css/style.css?202011281538",
         "/"
       ]);
     })
